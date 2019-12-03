4237aca491a0e84145618d71b3735bb036e2bba8
unknown <santos.santiagocob@itsva.edu.mx>
2019-12-03 11:26:00 -0600

    version 2

--- unidad-5.py
+++ unidad-5.py
@@ -15,32 +15,34 @@ for linea in archivo:
 print (texto)
 
 
-patron = re.compile('(\w+( |)=( |)[\d+\.\d+|\w]+)')
+patron = re.compile('[A-Za-z]\w*[ ]*[=][ ]*\w+[.]?\d*')
 result = re.findall(patron,texto)
 
 print("\n Resultado de busqueda, sentencias de asignación: ", result)
 
-patron2 = re.compile('\w.[\=]{1}\w*.\w*.[+|-|*|/]{1}\w*.\w*.\;')
+patron2 = re.compile('[A-Za-z]\w*[ ]*[=][ ]*\w+[.]?\d*[ ]*[+-\/\*][ ]*\w+')
 result2 = re.findall(patron2,texto)
 
 print("\n Resultado de busqueda, Operaciones aritméticas básicas: ", result2)
 
-patron3 = re.compile('[a-zA-A]+w*.[==|<=|>=|!=]{2}w*.[a-zA-Z0-9]')
+patron3 = re.compile('[A-Za-z]\w*[ ]*[<>!=][=][ ]*[A-Za-z0-9]|[A-Za-z]\w*[ ]*[<>][ ]*[A-Za-z0-9]')
 result3 = re.findall(patron3,texto)
 
 print("\n Resultado de busqueda, expresiones booleanas básicas:  ", result3)
 
 
-patron4 = re.compile('[a-zA-Z]{1,}[\d|\s]{1,}\=[\s|\w|\d.\d]{1,}[\s|\S][\|\/|\%|\+|\-][\s|\]\([\w|\d.\d]{1,}[\S|\s][\|\/|\%|\+|\-][\S|\s][\w|\d.\d]{1,}[ \)|\S\)]')
+#patron4 = re.compile('[A-Za-z]{1,}[\d|\s]{1,}\=[\s|\w|\d.\d]{1,}[\s|\S][\|\/|\%|\+|\-][\s|\]\([\w|\d.\d]{1,}[\S|\s][\|\/|\%|\+|\-][\S|\s][\w|\d.\d]{1,}[ \)|\S\)]')
+patron4 = re.compile('[A-Za-z][\w]*[ ]*[=][ ]*\w+[.]?\d*[ ]*[+-\/*%][ ]*\([ ]*\w+[.]?[\d]*[ ]*[+-\/*%][ ]*\w+[.]?[\d]*[ ]*\)')
 result4 = re.findall(patron4,texto)
 
 print("\n Resultado de busqueda, Formulas más complejas   ", result4)
 
 
-patron5 = re.compile('[for|if|while].[\(]\w.[\)]\{')
+patron5 = re.compile('if[ ]*\(.+\)|while[ ]*\(.+\)|for[ ]*\(.+\)')
 result5 = re.findall(patron5,texto)
 
 print("\n Resultado de busqueda, encabezado de estructuras de control:  ", result5)
+
 
 
 
